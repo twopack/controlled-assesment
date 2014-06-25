eb5b25b150de446587a4476bf7faabc8c0a00069
biggie <digbickbig@yahoo.com>
2014-06-25 09:22:35 +0100

    Update report.py

--- report.py
+++ report.py
@@ -1,4 +1,9 @@
 task one
 --------
-its is a currency that 1) has a currency the can be changed by the user regularly 
-2)
+Creating a currency converter that 1) has exchange rates that can be regularly changed by the user.
+2) user should be able to enter an amount, select chosen currnecy, and the currency to convert it into. 3) 
+figure shown should be to two decimal places.
+Currencys required: GBP (Great British Pound) EUR (Euro) USD (US Dollar) JPY (Japanese Yen)
+
+tasktwo
+-------
